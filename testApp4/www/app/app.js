12e41f6f1422c6d7faf5ec83208f31009dc320d5
Yashwanth S <yashwanth493@gmail.com>
2016-10-08 11:52:18 +0530

    added project

---
+++ testApp4/www/app/app.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("myapp", ["ionic", "myapp.controllers", "myapp.services"])
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+            .state("app", {
+                url: "/app",
+                abstract: true,
+                templateUrl: "app/templates/view-menu.html",
+                controller: "appCtrl"
+            })
+            .state("app.home", {
+                url: "/home",
+                templateUrl: "app/templates/view-home.html",
+                controller: "homeCtrl"
+            })
+            .state("register", {
+                url: "register",
+                templateUrl: "app/templates/register.html",
+                controller: "registerCtrl"
+            })
+              .state("view-login", {
+                  url: "view-login",
+                  templateUrl: "app/templates/view-home.html",
+                  controller: "registerCtrl"
+              })
+              .state("view-afterLogin", {
+                  url: "view-afterLogin",
+                  templateUrl: "app/templates/view-afterLogin.html",
+                  controller: "afterLoginCtrl"
+              })
+                  .state("view-forgetPassword", {
+                      url: "view-forgetPassword",
+                      templateUrl: "app/templates/view-forgetPassword.html",
+                      controller: "forgetPasswordCtrl"
+                  })
+            .state("view-getNewPassword", {
+                url: "view-getNewPassword",
+                templateUrl: "app/templates/view-getNewPassword.html",
+                controller: "getNewPasswordCtrl"
+            })
+           
+            $urlRouterProvider.otherwise("/app/home");
+        });
+})();
